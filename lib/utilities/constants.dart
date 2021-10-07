a8842a1f1e654a1b1a102d3d0126e19fa037809c
Artem Altunin <69789325+ArtemAlt@users.noreply.github.com>
2021-10-07 18:07:37 +0300

    Update constants.dart

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,5 +1,5 @@
 class Constants{
-  static const String WEATHER_KEY="1369dd6b5ae78fc9952261ab9aa236b4";
+  static const String WEATHER_KEY="################################";
   static const String WEATHER_BASE_SCHEMA="https://";
   static const String WEATHER_BASE_URL="api.openweathermap.org";
   static const String WEATHER_BASE_REQUEST = "data/2.5/forecast/daily";
